8702889174d77b67df59b460bac48e66c7965666
unknown <mateuszm@.FINEOS.com>
2009-03-18 16:37:20 +1100

    I jeszcze zmiany komunikatow, widokow i skryptow startowych

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,5 @@
 import pl.jaceklaskowski.nauczyciel.Pierwiastek
+import pl.jaceklaskowski.nauczyciel.Slowo
 
 class BootStrap {
 
@@ -6,6 +7,12 @@ class BootStrap {
          new Pierwiastek(symbol:'N', nazwa:'azot').save()
          new Pierwiastek(symbol:'P', nazwa:'fosfor').save()
          new Pierwiastek(symbol:'K', nazwa:'potas').save()
+         new Slowo(pytanie:'kot', odpowiedz:'cat').save()
+         new Slowo(pytanie:'pies', odpowiedz:'dog').save()
+         new Slowo(pytanie:'krowa', odpowiedz:'cow').save()
+         new Slowo(pytanie:'koń', odpowiedz:'horse').save()
+         new Slowo(pytanie:'ptak', odpowiedz:'bird').save()
+         new Slowo(pytanie:'ryba', odpowiedz:'fish').save()
      }
      def destroy = {
      }
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -10,5 +10,8 @@
         <p style="margin-left:20px;width:80%">
         <g:message code="nauczyciel.zaproszenie" /> <g:link controller="pytanie"><g:message code="nauczyciel.wchodz" /></g:link>
         </p>
+        <p style="margin-left:20px;width:80%">
+        <g:message code="nauczyciel.zaproszenieDoSlowek" /> <g:link controller="sprawdzian" action="zadaj"><g:message code="nauczyciel.wchodz" /></g:link>
+        </p>
     </body>
 </html>
